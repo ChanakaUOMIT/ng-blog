89ad57aad2b5c01dee70c12b361ba2b88b2bc2ee
ChanakaUOMIT <chanakauomfit@gmail.com>
2020-11-14 02:13:23 +0530

    added basic components

---
+++ src/app/artile-list/artile-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-artile-list',
+  templateUrl: './artile-list.component.html',
+  styleUrls: ['./artile-list.component.css']
+})
+export class ArtileListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
