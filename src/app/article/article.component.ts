08d797dc7ba5b1411c53ff07b500330af5cec7a7
ChanakaUOMIT <chanakauomfit@gmail.com>
2020-11-16 00:34:37 +0530

    shared service implemeted

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Article } from '../article';
 import { ArticleService } from '../services/article.service';
+import { SharedService } from '../services/shared.service';
 
 @Component({
   selector: 'app-article',
@@ -15,7 +17,9 @@ export class ArticleComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private articleServices: ArticleService,
-    private router: Router
+    private router: Router,
+    private titleService: Title,
+    private sharedService: SharedService
     ) { }
 
   ngOnInit() {
@@ -27,6 +31,7 @@ export class ArticleComponent implements OnInit {
           return;
         }
         this.article = article;
+        this.titleService.setTitle(`${this.article.title} - ${this.sharedService.blogTitle}`)
         console.log('get single article : ' , this.article)
       });
     });
--- src/app/artile-list/artile-list.component.ts
+++ src/app/artile-list/artile-list.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { Article } from '../article';
 import { ARTICLES } from '../mock-articles';
 import { ArticleService } from '../services/article.service';
+import { SharedService } from '../services/shared.service';
 
 @Component({
   selector: 'app-artile-list',
@@ -12,10 +14,16 @@ export class ArtileListComponent implements OnInit {
 
   arricles: Article[] = [];
 
-  constructor(private articleService: ArticleService) { }
+  constructor(
+    private articleService: ArticleService,
+    private titleService: Title,
+    private sharedService: SharedService
+    ) { }
 
   ngOnInit() {
     this.getArticles();
+    this.titleService.setTitle(` ${this.sharedService.blogTitle}`);
+
   }
 
   getArticles(): void {
