5e241e0eb5d65d0ded3a28ffa7d5c374fe527f25
ChanakaUOMIT <chanakauomfit@gmail.com>
2020-11-15 21:28:41 +0530

    param pass fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { ArticleComponent } from './article/article.component';
 import { ArtileListComponent } from './artile-list/artile-list.component';
 
 const routes: Routes = [
   { path: 'articles', component: ArtileListComponent},
   { path : 'about', component: AboutComponent},
-  { path : '', component: ArtileListComponent}
+  { path : '', component: ArtileListComponent},
+  { path : ':key', component: ArticleComponent}
+
 ];
 
 @NgModule({
